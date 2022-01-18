d6fac69900955696c786b7d9c50250f61ea2f51e
Yvon Momboisse <momboisse.yvn@gmail.com>
2022-01-18 22:33:17 +0100

    carousel clicks and modal

--- app.js
+++ app.js
@@ -1,24 +1,91 @@
 import { getBestMovies, getMovieById } from "./get.js";
+import { moveSlidesLeft, moveSlidesRight } from "./carousel.js";
 
-const setImageUrls = (id, arrayOfImages) => {
+const setImageDataAttribute = (element, attribute) => {
+  element.setAttribute("data-movie-id", attribute);
+};
+
+const handleMovieClick = (element) => {
+  element.onclick = async () => {
+    let movieId = element.getAttribute("data-movie-id");
+    let movie = await getMovieById(movieId);
+    let modal = document.getElementById("myModal");
+    console.log(movie);
+    modal.style.display = "block";
+  };
+};
+
+const setImageUrls = (id, movies) => {
   let imageContainer = document.getElementById(id);
   for (let i = 0; i < imageContainer.children.length; i++) {
-    imageContainer.children[i].src = arrayOfImages[i].image_url;
+    imageContainer.children[i].src = movies[i].image_url;
+    setImageDataAttribute(imageContainer.children[i], movies[i].id);
   }
 };
 
-const carouselStates = {
-  best_movies_of_all_time: false,
-  best_comedies: false,
-  best_action_movies: false,
-  best_adventure_movies: false,
-};
-
-const switchCarouselState = (carouselId) => {
-  carouselStates[carouselId] = !carouselStates[carouselId];
+const setImagesClickEvents = (id) => {
+  let imageContainer = document.getElementById(id);
+  for (let i = 0; i < imageContainer.children.length; i++) {
+    handleMovieClick(imageContainer.children[i]);
+  }
 };
 
 const main = async () => {
+  const bestMoviesAllTimeLeftButton = document.getElementById(
+      "best_movies_of_all_time_left_button"
+    ),
+    bestMoviesAllTimeRightButton = document.getElementById(
+      "best_movies_of_all_time_right_button"
+    ),
+    bestComediesLeftButton = document.getElementById(
+      "best_comedies_left_button"
+    ),
+    bestComediesRightButton = document.getElementById(
+      "best_comedies_right_button"
+    ),
+    bestActionMoviesLeftButton = document.getElementById(
+      "best_action_movies_left_button"
+    ),
+    bestActionMoviesRightButton = document.getElementById(
+      "best_action_movies_right_button"
+    ),
+    bestAdventureMoviesLeftButton = document.getElementById(
+      "best_adventure_movies_left_button"
+    ),
+    bestAdventureMoviesRightButton = document.getElementById(
+      "best_adventure_movies_right_button"
+    ),
+    modal = document.getElementById("myModal"),
+    closeModal = document.getElementsByClassName("close")[0];
+
+  closeModal.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  const moveBestMoviesAllTimeLeft = moveSlidesLeft("best_movies_of_all_time"),
+    moveBestMoviesAllTimeRight = moveSlidesRight("best_movies_of_all_time"),
+    moveBestComediesLeft = moveSlidesLeft("best_comedies"),
+    moveBestComediesRight = moveSlidesRight("best_comedies"),
+    moveBestActionLeft = moveSlidesLeft("best_action_movies"),
+    moveBestActionRight = moveSlidesRight("best_action_movies"),
+    moveBestAdventureLeft = moveSlidesLeft("best_adventure_movies"),
+    moveBestAdventureRight = moveSlidesRight("best_adventure_movies");
+
+  bestMoviesAllTimeLeftButton.onclick = moveBestMoviesAllTimeLeft;
+  bestMoviesAllTimeRightButton.onclick = moveBestMoviesAllTimeRight;
+  bestComediesLeftButton.onclick = moveBestComediesLeft;
+  bestComediesRightButton.onclick = moveBestComediesRight;
+  bestActionMoviesLeftButton.onclick = moveBestActionLeft;
+  bestActionMoviesRightButton.onclick = moveBestActionRight;
+  bestAdventureMoviesLeftButton.onclick = moveBestAdventureLeft;
+  bestAdventureMoviesRightButton.onclick = moveBestAdventureRight;
+
   const bestRatedMovies = await getBestMovies(
     "http://localhost:8000/api/v1/titles/?page_size=8&sort_by=-imdb_score,-votes"
   );
@@ -39,11 +106,17 @@ const main = async () => {
 
   let bestMovieImg = document.getElementById("best_movie_img");
   bestMovieImg.src = bestMovieEver.image_url;
+  setImageDataAttribute(bestMovieImg, bestMovieEver.id);
+  handleMovieClick(bestMovieImg);
 
   setImageUrls("best_movies_of_all_time", bestRatedMovies.slice(1));
+  setImagesClickEvents("best_movies_of_all_time", bestRatedMovies.slice(1));
   setImageUrls("best_comedies", bestComedies);
+  setImagesClickEvents("best_comedies", bestComedies);
   setImageUrls("best_action_movies", bestActionMovies);
+  setImagesClickEvents("best_action_movies", bestActionMovies);
   setImageUrls("best_adventure_movies", bestAdventureMovies);
+  setImagesClickEvents("best_adventure_movies", bestAdventureMovies);
 };
 
 window.onload = main;
--- carousel.js
+++ carousel.js
@@ -1,31 +1,20 @@
-let images = ["a", "b", "c", "d", "e", "f", "g"];
-let indexesToDisplay = [0, 1, 2];
-
-let display = (arrayOfIndexes) => {
-  for (idx of arrayOfIndexes) {
-    console.log(images[idx]);
-  }
+export const moveSlidesLeft = (slidesContainerId) => {
+  return () => {
+    console.log("hey");
+    const slidesContainer = document.getElementById(slidesContainerId);
+    const slides = slidesContainer.children;
+    let firstSlideClone = slides[0].cloneNode();
+    slidesContainer.appendChild(firstSlideClone);
+    slidesContainer.removeChild(slides[0]);
+  };
 };
 
-let calcNextIndexesToDisplay = (arrayOfIndexes, direction) => {
-  let indexesToDisplay = [];
-  let lastIndex = images.length - 1;
-  if (direction === "next") {
-    for (idx of arrayOfIndexes) {
-      if (idx + 1 > lastIndex) indexesToDisplay.push(0);
-      else indexesToDisplay.push(idx + 1);
-    }
-  } else if (direction === "prev") {
-    for (idx of arrayOfIndexes) {
-      if (idx - 1 < 0) indexesToDisplay.push(lastIndex);
-      else indexesToDisplay.push(idx - 1);
-    }
-  } else {
-    console.error("Direction argument should be either 'prev' or 'next'");
-  }
-  return indexesToDisplay;
+export const moveSlidesRight = (slidesContainerId) => {
+  return () => {
+    let slidesContainer = document.getElementById(slidesContainerId);
+    const slides = slidesContainer.children;
+    let lastSlideClone = slides[slides.length - 1].cloneNode();
+    slidesContainer.insertBefore(lastSlideClone, slides[0]);
+    slidesContainer.removeChild(slides[slides.length - 1]);
+  };
 };
-
-indexesToDisplay = calcNextIndexesToDisplay(indexesToDisplay, "next");
-indexesToDisplay = calcNextIndexesToDisplay(indexesToDisplay, "next");
-indexesToDisplay = calcNextIndexesToDisplay(indexesToDisplay, "next");
